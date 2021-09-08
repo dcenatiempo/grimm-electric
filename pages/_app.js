859530df003a5109bf4dcc28ad5b00abe7900fc4
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2021-09-07 18:10:17 -0600

    linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import 'styles/globals.css'
-import 'styles/reset.css'
+import 'styles/globals.css';
+import 'styles/reset.css';
 
 function MyApp({ Component, pageProps }) {
-    // https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
-    const Layout = Component.layout || (children => <>{children}</>)
+  // https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
+  const Layout = Component.layout || ((children) => <>{children}</>);
 
-    return (
-      <Layout>
-        <Component {...pageProps}></Component>
-      </Layout>
-    )
+  return (
+    <Layout>
+      <Component {...pageProps}></Component>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import styles from 'styles/Home.module.css'
-import MainLayout from 'components/layouts/MainLayout'
-import Button from 'components/base/Button'
+import Head from 'next/head';
+import styles from 'styles/Home.module.css';
+import MainLayout from 'components/layouts/MainLayout';
+import Button from 'components/base/Button';
 
 export default function Home() {
   return (
@@ -13,13 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Grimm Electric
-        </h1>
-        <Button color="blue" label="nothing will happen if you press me"/>
-     </main>
+        <h1 className={styles.title}>Welcome to Grimm Electric</h1>
+        <Button color="blue" label="nothing will happen if you press me" />
+      </main>
     </div>
-  )
+  );
 }
 
 Home.layout = MainLayout;
